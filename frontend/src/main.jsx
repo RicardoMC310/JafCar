91bb255f08622f915b74ec750a149d4ba67231d6
RicardoMC310 <ricardo.matos72.etec@gmail.com>
2025-06-30 00:50:18 -0300

    backend feito

---
+++ frontend/src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from './Routes/Home.jsx';
+import Login from './Routes/Login.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {path: "/", element: <Home />},
+      {path: "/", element: <Login />},
+    ]
+  }
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
